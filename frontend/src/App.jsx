4a586c92a301367d0e8f9bf28fea49fce6b37b1e
Huy Nguyen <huy.h.nguy@gmail.com>
2024-07-29 16:39:09 -0500

    center image on page and size it responsively

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import waldoPic from "./assets/wheres_waldo_pic.webp"
+import "./index.css"
 
 function App() {
 
   return (
     <>
       <h1>hello</h1>
-      <img src={waldoPic} alt="" />
+      <img src={waldoPic} alt="" className="picture"/>
     </>
   )
 }
