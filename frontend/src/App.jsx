3671ab245640fa5c34b73892b02e7e5a0d4f2681
Huy Nguyen <huy.h.nguy@gmail.com>
2024-07-29 16:22:35 -0500

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import waldoPic from "./assets/wheres_waldo_pic.webp"
+
+function App() {
+
+  return (
+    <>
+      <h1>hello</h1>
+      <img src={waldoPic} alt="" />
+    </>
+  )
+}
+
+export default App
