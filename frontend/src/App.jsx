5577bc2ad4ce2947d07985ea055cdd339dd9b8ac
Huy Nguyen <huy.h.nguy@gmail.com>
2024-07-29 17:32:14 -0500

    add . pop up when user clicks image

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,23 @@ import waldoPic from "./assets/wheres_waldo_pic.webp"
 import "./index.css"
 
 function App() {
+  function handleClick(e) {
+    console.log(e);
+    const targetingBox = document.querySelector(".targeting-box");
+    console.log(targetingBox);
+    targetingBox.showModal();
+  }
 
   return (
     <>
-      <h1>hello</h1>
-      <img src={waldoPic} alt="" className="picture"/>
+      <h1 className="title">Where's Waldo?</h1>
+      <main>
+        <img src={waldoPic} alt="" className="picture" onClick={(e) => {handleClick(e)}}/>
+        <dialog className="targeting-box">
+          <h2>Hello.</h2>
+        </dialog>
+      </main>
+
     </>
   )
 }
