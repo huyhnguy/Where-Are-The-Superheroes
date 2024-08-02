ea60015ccc0cd91e40448651b62ee5668c68582c
Huy Nguyen <huy.h.nguy@gmail.com>
2024-08-01 22:48:15 -0500

    complete script to populate database with characters and their coordinate ranges

--- backend/populatedb.js
+++ backend/populatedb.js
@@ -1,10 +1,17 @@
-const Character = "./models/character.js";
+#! /usr/bin/env node
+
+const Character = require("./models/character.js");
 
 const characters = [];
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = process.env.MONGODB_URL;
+const userArgs = process.argv.slice(2);
+
+const mongoDB = userArgs[0];
+
+main().catch((err) => console.log(err));
+
 async function main() {
     console.log("Debug: About to connect");
     await mongoose.connect(mongoDB);
@@ -14,9 +21,25 @@ async function main() {
     mongoose.connection.close();
 }
 
-async function characterCreate({ name, xmin, xmax, ymin, ymax }) {
+async function characterCreate(index, name, xmin, xmax, ymin, ymax) {
     const character = new Character({
+        name: name,
+        x_min: xmin,
+        x_max: xmax,
+        y_min: ymin,
+        y_max: ymax
+    });
 
-        x_min: 
-    })
+    await character.save();
+    characters[index] = character;
+    console.log(`added character: ${name}`);
+}
+
+async function createCharacters() {
+    console.log('adding all characters');
+    await Promise.all([
+        characterCreate(0, "Deadpool", 0.73, 0.75, 0.72, 0.77),
+        characterCreate(1, "Flash", 0.42, 0.44, 0.59, 0.63),
+        characterCreate(2, "Spiderman", 0.75, 0.77, 0.18, 0.21),
+    ])
 }
