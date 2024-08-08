9c6fd437deb894c1123805d299b0dd9304a885a8
Huy Nguyen <huy.h.nguy@gmail.com>
2024-08-08 00:28:47 -0500

    update styling of hint box

--- frontend/src/Hint.jsx
+++ frontend/src/Hint.jsx
@@ -17,7 +17,7 @@ export default function Hint({ startHintTimer, characterCoordinates }) {
     function chooseHint(hintArray) {
         for (let i = 0; i < hintArray.length; i++) {
             if (hintArray[i] === null) {
-                if (i === 0) { return "Deadpool is near the Gadget Zone." }
+                if (i === 0) { return "Deadpool is near the Gadget Zone" }
                 else if (i === 1) { return "Flash is near the FREAKSHOW Collectibles" }
                 else if (i === 2) { return "Spiderman is near the iDeathNoteBook Air Demo" }
             }
@@ -25,10 +25,10 @@ export default function Hint({ startHintTimer, characterCoordinates }) {
     }
 
     return(
-        <div className="hint-popup" style={{ visibility: giveHint === false && "hidden", pointerEvents: giveHint === false && "none" }}>
+        <div className="hint-popup" style={{ visibility: giveHint === false && "hidden", pointerEvents: giveHint === false && "none", display: giveHint && "flex", gap: giveHint && "10px" }}>
             {giveHint === null && 
                 <>
-                    <p>Would you like a hint?</p>
+                    <p style={{marginTop: "0"}}>Would you like a hint?</p>
                     <div className="hint-buttons">
                         <button onClick={handleClose}>No</button>
                         <button onClick={handleYes}>Yes</button>
@@ -37,12 +37,13 @@ export default function Hint({ startHintTimer, characterCoordinates }) {
             }
             {giveHint &&
                 <>
+
+                    <p style={{margin: "0"}}>
+                        {chooseHint(characterCoordinates)}
+                    </p>
                     <button className="close-button" onClick={handleClose}>
                         <FontAwesomeIcon icon={faXmark} />
                     </button>
-                    <p>
-                        {chooseHint(characterCoordinates)}
-                    </p>
                 </>
 
             }
