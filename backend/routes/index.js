4f4183da81b9e25ebdfa80b12c486ad051d88b4d
Huy Nguyen <huy.h.nguy@gmail.com>
2024-08-09 15:44:39 -0500

    change path to home page file

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile(
-    path.join(__dirname, "../../frontend/index.html"),
+    path.join(__dirname, "../frontend/index.html"),
     function (err) {
       if (err) {
         res.status(500).send(err);
