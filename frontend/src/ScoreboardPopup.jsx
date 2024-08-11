e20314be2b5e62694f24b70df7cc75acf89d32f6
Huy Nguyen <huy.h.nguy@gmail.com>
2024-08-10 22:00:31 -0500

    fix url to submit post request for score

--- frontend/src/ScoreboardPopup.jsx
+++ frontend/src/ScoreboardPopup.jsx
@@ -39,7 +39,7 @@ export default function ScoreboardPopup() {
             time: duration,
         });
 
-        fetch('https://wheres-waldo-backend-531ef25fc781.herokuapp.com/scoreboard/score', {
+        fetch('https://wheres-waldo-backend-531ef25fc781.herokuapp.com/api/scoreboard/score', {
             method: 'POST',
             headers: {
               'Accept': 'application/json',
